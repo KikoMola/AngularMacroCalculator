05d74044bbe1910d2a7687fd5182b006e24e6464
Kiko <kikoguirado@gmail.com>
2022-04-27 20:24:54 +0200

    fixed chart

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,38 +1,38 @@
 import { Component, ViewChild } from '@angular/core';
 import { ChartData, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
-  gramosProtes: number = 0;
-  gramosCarbos: number = 0;
-  gramosGrasas: number = 0;
+  gProtein: number = 0;
+  gCarbs: number = 0;
+  gFat: number = 0;
 
   public doughnutChartLabels: string[] = [ 'Proteína', 'Carbohidratos', 'Grasa' ];
   public doughnutChartData: ChartData<'doughnut'> = {
     labels: this.doughnutChartLabels,
     datasets: [
-      { data: [ this.gramosProtes, this.gramosCarbos, this.gramosGrasas ] },
+      { data: [ this.gProtein, this.gCarbs, this.gFat ] },
     ]
   };
   public doughnutChartType: ChartType = 'doughnut';
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
-  addMeal(gramosTotales: HTMLInputElement, gramosProteina: HTMLInputElement, gramosCarbohidratos: HTMLInputElement, gramosGrasa: HTMLInputElement) {
-    this.gramosProtes = gramosProteina.valueAsNumber;
-    this.gramosCarbos = gramosCarbohidratos.valueAsNumber;
-    this.gramosGrasas = gramosGrasa.valueAsNumber;
-    gramosTotales.value = ''
-    gramosProteina.value = ''
-    gramosCarbohidratos.value = ''
-    gramosGrasa.value = ''
-    gramosTotales.focus()  
-    this.chart?.chart?.update();    
+  addMeal(totalGrams: HTMLInputElement, proteinGrams: HTMLInputElement, fatGrams: HTMLInputElement, carbGrams: HTMLInputElement) {
+    this.doughnutChartData.datasets[0].data[0] = parseInt(proteinGrams.value);
+    this.doughnutChartData.datasets[0].data[1] = parseInt(carbGrams.value);
+    this.doughnutChartData.datasets[0].data[2] = parseInt(fatGrams.value);
+    this.chart?.chart?.update();
+    totalGrams.value = ''
+    proteinGrams.value = ''
+    fatGrams.value = ''
+    carbGrams.value = ''
+    totalGrams.focus()
+        
     return false;
   }
 
