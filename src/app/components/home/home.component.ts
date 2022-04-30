20e62f3acff46060c75da822bdf93f97b7b9616c
Kiko <kikoguirado@gmail.com>
2022-04-30 21:22:47 +0200

    added calories field

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,36 +4,50 @@ import { BaseChartDirective } from 'ng2-charts';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
-  gProtein: number = 0;
-  gCarbs: number = 0;
-  gFat: number = 0;
-
-  public doughnutChartLabels: string[] = [ 'Proteína', 'Carbohidratos', 'Grasa' ];
+  public doughnutChartLabels: string[] = [
+    'Calorías',
+    'Proteína',
+    'Carbohidratos',
+    'Grasa',
+  ];
   public doughnutChartData: ChartData<'doughnut'> = {
     labels: this.doughnutChartLabels,
     datasets: [
-      { data: [ this.gProtein, this.gCarbs, this.gFat ] },
-    ]
+      {
+        data: [0, 0, 0, 0],
+        backgroundColor: ['#7DFF93', '#36A2EB', '#FFCE56', '#FF6384'],
+        hoverBackgroundColor: '#E6E6E6',
+        hoverBorderColor: '#E6E6E6',
+      },
+    ],
   };
   public doughnutChartType: ChartType = 'doughnut';
   @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
-  addMeal(totalGrams: HTMLInputElement, proteinGrams: HTMLInputElement, fatGrams: HTMLInputElement, carbGrams: HTMLInputElement) {
-    this.doughnutChartData.datasets[0].data[0] = parseInt(proteinGrams.value);
-    this.doughnutChartData.datasets[0].data[1] = parseInt(carbGrams.value);
-    this.doughnutChartData.datasets[0].data[2] = parseInt(fatGrams.value);
+  addMeal(
+    totalGrams: HTMLInputElement,
+    totalCals: HTMLInputElement,
+    proteinGrams: HTMLInputElement,
+    fatGrams: HTMLInputElement,
+    carbGrams: HTMLInputElement
+  ) {
+    this.doughnutChartData.datasets[0].data[0] =
+      parseInt(totalCals.value) * (parseInt(totalGrams.value) / 100);
+    this.doughnutChartData.datasets[0].data[1] =
+      parseInt(proteinGrams.value) * (parseInt(totalGrams.value) / 100);
+    this.doughnutChartData.datasets[0].data[2] =
+      parseInt(carbGrams.value) * (parseInt(totalGrams.value) / 100);
+    this.doughnutChartData.datasets[0].data[3] =
+      parseInt(fatGrams.value) * (parseInt(totalGrams.value) / 100);
     this.chart?.chart?.update();
-    totalGrams.value = ''
-    proteinGrams.value = ''
-    fatGrams.value = ''
-    carbGrams.value = ''
-    totalGrams.focus()
-        
+    totalGrams.value = '';
+    totalCals.value = '';
+    proteinGrams.value = '';
+    fatGrams.value = '';
+    carbGrams.value = '';
     return false;
   }
-
 }
